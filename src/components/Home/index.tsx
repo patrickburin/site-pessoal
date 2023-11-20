26e3c2e632a1ba0c675da12eac1a020a05b278bf
Patrick <patrickburin17@gmail.com>
2023-11-20 17:56:23 -0300

    Início da pagina

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,23 @@ import * as C from "./styles";
 const Home = () => {
   return (
     <C.Container>
-      <div>aloo</div>
+      <div className="name">Patrick Burin Rodriguez</div>
+      <C.Description>
+        <img src="src/images/profile.png" className="image" />
+        <div className="description">
+          Olá! Meu nome é Patrick Burin Rodriguez e sou desenvolvedor front end.
+          Nasci em 2000 no Brasil no sul de Santa Catarina, mais específicamente
+          em Araranguá. Atualmente estou cursando a 7ª fase de Ciência da
+          Computação na UNESC, em Criciúma. Atuo como desenvolvedor front end no
+          Unesc Labs, sendo uma parceria entre a universidade e empresas de
+          fora. Sou apaixonado por esportes, principalmente futebol e handeboll
+          e também gosto de alguns eSports, como o famoso League of Legends. No
+          meu tempo livre eu gosto de praticar esportes e jogar uma solo/duo no
+          Lol. Também amo a praia e por morar em cidade litorânea, sempre que
+          posso passo por lá.
+        </div>
+      </C.Description>
+      <C.Experiences>alooooooo</C.Experiences>
     </C.Container>
   );
 };
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -2,7 +2,41 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
-  background-color: red;
+  background: #272727;
+  color: #fff;
+  gap: 20px;
+
+  .name {
+    background: linear-gradient(to bottom, red, black);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+    font-size: 70px;
+    text-align: center;
+  }
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 70px;
+  align-items: center;
+
+  .image {
+    background-color: #3f3f3f;
+    border-radius: 30px;
+  }
+  .description {
+    width: 40%;
+    text-align: justify;
+  }
+`;
+
+export const Experiences = styled.div`
+  display: flex;
+  background-color: #3f3f3f;
 `;
