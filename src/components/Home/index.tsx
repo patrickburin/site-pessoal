dc7c064b0affaa7fc54c14e8432cd0487b39f27f
Patrick <patrickburin17@gmail.com>
2023-11-21 16:54:37 -0300

    Desenvolvimento do site pessoal

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,25 +1,121 @@
 import * as C from "./styles";
+import { TfiLink } from "react-icons/tfi";
 
 const Home = () => {
   return (
     <C.Container>
       <div className="name">Patrick Burin Rodriguez</div>
-      <C.Description>
-        <img src="src/images/profile.png" className="image" />
-        <div className="description">
-          Olá! Meu nome é Patrick Burin Rodriguez e sou desenvolvedor front end.
-          Nasci em 2000 no Brasil no sul de Santa Catarina, mais específicamente
-          em Araranguá. Atualmente estou cursando a 7ª fase de Ciência da
-          Computação na UNESC, em Criciúma. Atuo como desenvolvedor front end no
-          Unesc Labs, sendo uma parceria entre a universidade e empresas de
-          fora. Sou apaixonado por esportes, principalmente futebol e handeboll
-          e também gosto de alguns eSports, como o famoso League of Legends. No
-          meu tempo livre eu gosto de praticar esportes e jogar uma solo/duo no
-          Lol. Também amo a praia e por morar em cidade litorânea, sempre que
-          posso passo por lá.
+      <div className="text">
+        <C.Description>
+          <img src="src/images/profile.png" className="image" />
+          <div className="description">
+            Olá! Meu nome é Patrick Burin Rodriguez e sou desenvolvedor front
+            end. Nasci em 2000 no Brasil no sul de Santa Catarina, mais
+            específicamente em Araranguá. Atualmente estou cursando a 7ª fase de
+            Ciência da Computação na UNESC, em Criciúma. Atuo como desenvolvedor
+            front end no Unesc Labs, sendo uma parceria entre a universidade e
+            empresas de fora. Sou apaixonado por esportes, principalmente
+            futebol e handeboll e também gosto de alguns eSports, como o famoso
+            League of Legends. No meu tempo livre eu gosto de praticar esportes
+            e jogar uma solo/duo no Lol. Também amo a praia e por morar em
+            cidade litorânea, sempre que posso passo por lá.
+          </div>
+        </C.Description>
+        <div className="experiences">
+          <C.Experiences>
+            <div className="experiencesTitle">Minhas experiências</div>
+            <div className="job">
+              <div className="titleJob">
+                Desenvolvedor front end na OAIS Cloud / Unesc Labs
+              </div>
+              <div className="date">Fevereiro 2023 - Trabalho atual</div>
+              <div className="descriptionJob">
+                O <strong>Unesc Labs</strong> é um projeto da Unesc que visa a
+                integração com empresas de fora. Utilizando os equipamentos da
+                própria universidade a troco de experiência para os alunos.{" "}
+                <br />
+                Dentro do Unesc Labs eu desenvolvo para a OAIS Cloud, uma
+                empresa localizada no Morro da Fumaça e que utiliza a tecnologia
+                da empresa norueguesa PIQL para preservação de dados digitais
+                por longos períodos de tempo. Atuo no desenvolvimento do portal
+                da OAIS, onde os usuários têm acesso a documentos e assinatura
+                digital. <br />
+                <br />
+                <strong>Tecnologias:</strong> TypeScript, React,
+                Styled-Components, Material UI, Git, GitHub, Figma.
+              </div>
+            </div>
+            <hr />
+            <div className="job">
+              <div className="titleJob">
+                Suporte técnico - Consolide Registro de Marcas
+              </div>
+              <div className="date">Fevereiro 2021 - Março 2022</div>
+              <div className="descriptionJob">
+                Atuando nas tarefas diárias de um suporte técnico, como
+                atendimento ao cliente interno (outros funcionários da empresa)
+                na resolução de problemas com equipamentos técnicos e também de
+                software. <br />
+                Também fazia o intermedio entre os funcionários e o time de
+                desenvolvimento, ajudando os desenvolvedores e também o time de
+                produto da empresa.
+                <br /> <br />
+                <strong>Tecnologias:</strong> Notion, Figma, Trello.
+              </div>
+            </div>
+          </C.Experiences>
         </div>
-      </C.Description>
-      <C.Experiences>alooooooo</C.Experiences>
+        <div className="experiences">
+          <C.Projects>
+            <div className="projectsTitle">Projetos pessoais</div>
+            <div className="divider">
+              <div className="project">
+                <div className="titleProject">
+                  Site Estofaria do Ananias <TfiLink size={30} />
+                </div>
+                <div className="descriptionProject">
+                  Desenvolvimento de um site para uma estofaria automotiva.{" "}
+                  <br />
+                  <div className="tools">
+                    <div className="tool">React</div>
+                    <div className="tool">TypeScript</div>
+                    <div className="tool">Styled-Components</div>
+                    <div className="tool">Material UI</div>
+                  </div>
+                </div>
+                <div className="buttons">
+                  <strong>Disponível em:</strong>
+                  <button>Vercel</button>
+                  <button>GitHub</button>
+                </div>
+              </div>
+
+              <div className="project">
+                <div className="titleProject">
+                  Jogo adivinhar a bandeira <TfiLink size={30} />
+                </div>
+                <div className="descriptionProject">
+                  Desenvolvimento de um mini jogo onde o usuário deve adivinhar
+                  a bandeira. <br />
+                  <div className="tools">
+                    <div className="tool">Java</div>
+                    <div className="tool">Spring Boot </div>
+                    <div className="tool">React</div>
+                    <div className="tool">Typescript</div>
+                    <div className="tool">Material UI</div>
+                    <div className="tool">Styled-Component</div>
+                  </div>
+                </div>
+                <div className="buttons">
+                  <strong>Disponível em:</strong>
+                  <button>Vercel</button>
+                  <button>GitHub</button>
+                </div>
+              </div>
+            </div>
+          </C.Projects>
+        </div>
+      </div>
     </C.Container>
   );
 };
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -3,19 +3,28 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
   background: #272727;
   color: #fff;
   gap: 20px;
+  font-size: 18px;
+  padding-bottom: 15px;
 
   .name {
-    background: linear-gradient(to bottom, red, black);
-    -webkit-background-clip: text;
-    background-clip: text;
-    color: transparent;
-    font-size: 70px;
+    color: #808080;
+    font-size: 62px;
     text-align: center;
+    font-weight: bold;
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    gap: 100px;
+  }
+
+  .experiences {
+    display: flex;
+    justify-content: center;
   }
 `;
 
@@ -23,7 +32,7 @@ export const Description = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  gap: 70px;
+  gap: 50px;
   align-items: center;
 
   .image {
@@ -31,12 +40,133 @@ export const Description = styled.div`
     border-radius: 30px;
   }
   .description {
-    width: 40%;
+    width: 550px;
     text-align: justify;
   }
 `;
 
 export const Experiences = styled.div`
   display: flex;
+  flex-direction: column;
   background-color: #3f3f3f;
+  width: 1000px;
+  border-radius: 10px;
+  padding: 15px 30px;
+  gap: 5px;
+  box-shadow: rgba(0, 0, 0, 0.25) 6px 6px 80px 8px;
+
+  .experiencesTitle {
+    color: #808080;
+    font-size: 38px;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .job {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  .titleJob {
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .date {
+    font-size: 16px;
+    font-weight: lighter;
+  }
+
+  .descriptionJob {
+    background-color: #272727;
+    border-radius: 10px;
+    font-size: 16px;
+    padding: 10px;
+  }
+
+  hr {
+    width: 100%;
+  }
+`;
+
+export const Projects = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  background-color: #3f3f3f;
+  border-radius: 10px;
+  padding: 15px;
+  gap: 5px;
+  box-shadow: rgba(0, 0, 0, 0.25) 6px 6px 80px 8px;
+
+  .projectsTitle {
+    color: #808080;
+    font-size: 38px;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .divider {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+
+  .project {
+    background-color: #272727;
+    display: flex;
+    width: 500px;
+    flex-direction: column;
+    border-radius: 10px;
+    font-size: 16px;
+    padding: 10px 15px;
+    gap: 5px;
+  }
+
+  .titleProject {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .tools {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  .tool {
+    padding: 5px;
+    border-radius: 10px;
+    color: #fff;
+    background-color: #3f3f3f;
+  }
+
+  .buttons {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    flex-direction: row;
+    margin-top: 10px;
+  }
+
+  button {
+    color: #272727;
+    border-radius: 10px;
+    border: none;
+    font-weight: bold;
+    padding: 10px;
+    font-size: 16px;
+    background-color: #fff;
+    cursor: pointer;
+  }
 `;
