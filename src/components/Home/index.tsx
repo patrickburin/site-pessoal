13ab9beffe1c9420bb8f8364207701ee3231aeee
Patrick <patrickburin17@gmail.com>
2023-11-22 15:58:03 -0300

    Ajustes texto de descrição

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -10,16 +10,13 @@ const Home = () => {
         <C.Description>
           <img src={profile} className="image" />
           <div className="description">
-            Olá! Meu nome é Patrick Burin Rodriguez e sou desenvolvedor front
-            end. Nasci em 2000 no Brasil no sul de Santa Catarina, mais
-            específicamente em Araranguá. Atualmente estou cursando a 7ª fase de
-            Ciência da Computação na UNESC, em Criciúma. Atuo como desenvolvedor
-            front end no Unesc Labs, sendo uma parceria entre a universidade e
-            empresas de fora. Sou apaixonado por esportes, principalmente
-            futebol e handeboll e também gosto de alguns eSports, como o famoso
-            League of Legends. No meu tempo livre eu gosto de praticar esportes
-            e jogar uma solo/duo no Lol. Também amo a praia e por morar em
-            cidade litorânea, sempre que posso passo por lá.
+            Olá, meu nome é Patrick, tenho 23 anos e nasci em Araranguá, no sul
+            de Santa Catarina - Brasil. Estou cursando a 7ª fase de Ciências da
+            Computação na UNESC, em Críciúma-SC. Sou desenvolvedor front end no
+            momento e desejo me tornar full stack ou mobile. No meu tempo livre
+            eu gosto de coisas bem diversificadas, como praticar esportes e ir
+            para a academia mas também assistir séries e esportes. Também gosto
+            bastante de praia, viajar e principalmente sair com meus amigos.
           </div>
         </C.Description>
         <div className="experiences">
