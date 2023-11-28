710dc8a6be0689c6f29ec39c382fdb3e45b4119f
Patrick <patrickburin17@gmail.com>
2023-11-28 17:42:40 -0300

    Alteração da pagina grande

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -18,7 +18,7 @@ const Home = () => {
   return (
     <C.Container>
       <C.Start>
-        <C.Name>Patrick Burin Rodriguez</C.Name>
+        <div className="name">Patrick Burin Rodriguez</div>
 
         <C.Developer>
           Desenvolvedor
@@ -47,118 +47,18 @@ const Home = () => {
         </C.SocialMedia>
       </C.Start>
 
-      <div className="text">
-        <div className="imageText">
-          <C.Description>
-            <img src={profile} className="image" />
-            <div className="description">
-              Olá! Meu nome é Patrick e sou estudande de Ciências da Computação,
-              na Unesc. Atualmente sou desenvolvedor front end na OAIS Cloud e
-              trabalho com TypeScript e React. Fora do mundo da programação, eu
-              gosto bastante de ir à academia, jogar bola e handebol. Também
-              assisto bastante série e filmes. Gosto bastante de estar com minha
-              família e meus amigos, para fazer desde um churrasco até sair para
-              uma festa. Gosto muito de ir à praia e por morar em litoral, acabo
-              aproveitando bastante o mar, calor e o verão.
-            </div>
-          </C.Description>
-        </div>
+      <C.Experiences>
+        <img src={profile} className="image" />
 
-        <div className="experiences">
-          <C.Experiences>
-            <div className="experiencesTitle">Minhas experiências</div>
-            <div className="job">
-              <div className="titleJob">
-                Desenvolvedor front end na OAIS Cloud / Unesc Labs
-              </div>
-              <div className="date">Fevereiro 2023 - Trabalho atual</div>
-              <div className="descriptionJob">
-                O <strong>Unesc Labs</strong> é um projeto da Unesc que visa a
-                integração com empresas de fora. Utilizando os equipamentos da
-                própria universidade a troco de experiência para os alunos.{" "}
-                <br />
-                Dentro do Unesc Labs eu desenvolvo para a OAIS Cloud, uma
-                empresa localizada no Morro da Fumaça e que utiliza a tecnologia
-                da empresa norueguesa PIQL para preservação de dados digitais
-                por longos períodos de tempo. Atuo no desenvolvimento do portal
-                da OAIS, onde os usuários têm acesso a documentos e assinatura
-                digital.
-                <div className="tools">
-                  <strong>Tecnologias:</strong>
-                  <div className="tool">React</div>
-                  <div className="tool">TypeScript</div>
-                  <div className="tool">Styled-Components</div>
-                  <div className="tool">Material UI</div>
-                </div>
-              </div>
-            </div>
-            <hr />
-            <div className="job">
-              <div className="titleJob">
-                Suporte técnico - Consolide Registro de Marcas
-              </div>
-              <div className="date">Fevereiro 2021 - Março 2022</div>
-              <div className="descriptionJob">
-                Atuando nas tarefas diárias de um suporte técnico, como
-                atendimento ao cliente interno (outros funcionários da empresa)
-                na resolução de problemas com equipamentos técnicos e também de
-                software. <br />
-                Também fazia o intermedio entre os funcionários e o time de
-                desenvolvimento, ajudando os desenvolvedores e também o time de
-                produto da empresa.
-                <div className="tools">
-                  <strong>Tecnologias:</strong>
-                  <strong>Tecnologias:</strong>
-                  <div className="tool">Figma</div>
-                  <div className="tool">Trello</div>
-                </div>
-              </div>
-            </div>
-          </C.Experiences>
+        <div className="descripton">
+          <div className="title">Olá, eu sou o Patrick!</div>
+          Estou cursando a 7ª fase de Ciência da Computação e minha vida pessoal
+          é bastante variada. Gosto bastante de treinar e esportes,
+          principalmente futebol, handebol e vôlei. Também gosto de jogar alguns
+          eSports e assistir séries e filmes. Tenho o sonho de viajar pelo mundo
+          conhecendo vários países e culturas diferentes.
         </div>
-        <div className="experiences">
-          <C.Projects>
-            <div className="projectsTitle">Projetos pessoais</div>
-            <div className="divider">
-              <div className="project">
-                <div className="titleProject">
-                  Site Estofaria do Ananias <TfiLink size={30} />
-                </div>
-                <div className="tools">
-                  <div className="tool">React</div>
-                  <div className="tool">TypeScript</div>
-                  <div className="tool">Styled-Components</div>
-                  <div className="tool">Material UI</div>
-                </div>
-                <div className="buttons">
-                  <strong>Disponível em:</strong>
-                  <button>Vercel</button>
-                  <button>GitHub</button>
-                </div>
-              </div>
-
-              <div className="project">
-                <div className="titleProject">
-                  Jogo adivinhar a bandeira <TfiLink size={30} />
-                </div>
-                <div className="tools">
-                  <div className="tool">Java</div>
-                  <div className="tool">Spring Boot </div>
-                  <div className="tool">React</div>
-                  <div className="tool">Typescript</div>
-                  <div className="tool">Material UI</div>
-                  <div className="tool">Styled-Component</div>
-                </div>
-                <div className="buttons">
-                  <strong>Disponível em:</strong>
-                  <button>Vercel</button>
-                  <button>GitHub</button>
-                </div>
-              </div>
-            </div>
-          </C.Projects>
-        </div>
-      </div>
+      </C.Experiences>
     </C.Container>
   );
 };
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -4,41 +4,17 @@ import { device } from "../../device";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   background: #000;
   color: #fff;
   gap: 20px;
   font-size: 18px;
   padding-bottom: 15px;
 
-  .name {
-    color: #808080;
-    font-size: 62px;
-    text-align: center;
+  .title {
+    font-size: 40px;
     font-weight: bold;
-  }
-
-  .text {
-    display: flex;
-    flex-direction: column;
-    gap: 100px;
-  }
-
-  .experiences {
-    display: flex;
-    justify-content: center;
-    min-height: 100vh;
-  }
-
-  .imageText {
-    display: flex;
-    justify-content: center;
-    min-height: 100vh;
-  }
-
-  @media ${device.tablet} {
-    .name {
-      font-size: 40px;
-    }
+    text-align: center;
   }
 `;
 
@@ -49,12 +25,13 @@ export const Start = styled.div`
   justify-content: center;
   gap: 50px;
   height: 100vh;
-`;
 
-export const Name = styled.div`
-  text-align: center;
-  color: #fff;
-  font-size: 70px;
+  .name {
+    text-align: center;
+    color: #fff;
+    font-weight: normal;
+    font-size: 70px;
+  }
 `;
 
 export const Developer = styled.div`
@@ -78,217 +55,23 @@ export const SocialMedia = styled.div`
   }
 `;
 
-export const Description = styled.div`
-  background-color: #3f3f3f;
-  display: flex;
-  flex-direction: row;
-  height: min-content;
-  width: 80%;
-  justify-content: center;
-  gap: 20px;
-  align-items: center;
-  border-radius: 10px;
-  padding: 10px 20px;
-
-  .image {
-    background-color: #3f3f3f;
-    border-radius: 50%;
-    border: 2px solid #808080;
-  }
-  .description {
-    text-align: justify;
-  }
-
-  @media ${device.tablet} {
-    flex-direction: column;
-
-    .description {
-      font-size: 22px;
-    }
-  }
-`;
-
 export const Experiences = styled.div`
   display: flex;
-  flex-direction: column;
-  background-color: #3f3f3f;
-  width: 80%;
-  height: min-content;
-  border-radius: 10px;
+  flex-direction: row;
+  width: 90%;
+  align-items: center;
+  min-height: 100vh;
+  font-size: 24px;
   padding: 20px;
-  gap: 5px;
-  box-shadow: rgba(0, 0, 0, 0.25) 6px 6px 80px 8px;
+  gap: 30px;
 
-  .experiencesTitle {
-    color: #fff;
-    font-size: 38px;
-    font-weight: bold;
-    text-align: center;
-  }
-
-  .job {
+  .descripton {
     display: flex;
     flex-direction: column;
     gap: 5px;
   }
 
-  .titleJob {
-    font-size: 20px;
-    font-weight: bold;
-  }
-
-  .date {
-    font-size: 16px;
-    font-weight: lighter;
-  }
-
-  .descriptionJob {
-    background-color: #272727;
-    border-radius: 10px;
-    font-size: 16px;
-    padding: 10px;
-  }
-
-  hr {
-    width: 100%;
-  }
-
-  .tools {
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
-    flex-wrap: wrap;
-    align-items: center;
-    margin-top: 10px;
-  }
-
-  .tool {
-    padding: 5px;
-    border-radius: 10px;
-    color: #fff;
-    background-color: #3f3f3f;
-  }
-
-  @media ${device.tablet} {
-    .experiencesTitle {
-      font-size: 28px;
-    }
-
-    .titleJob {
-      font-size: 22px;
-    }
-
-    .date {
-      font-size: 20px;
-    }
-
-    .descriptionJob {
-      font-size: 20px;
-    }
-  }
-`;
-
-export const Projects = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-color: #3f3f3f;
-  border-radius: 10px;
-  width: 80%;
-  height: min-content;
-  padding: 20px;
-  gap: 5px;
-  box-shadow: rgba(0, 0, 0, 0.25) 6px 6px 80px 8px;
-
-  .projectsTitle {
-    color: #fff;
-    font-size: 38px;
-    font-weight: bold;
-    text-align: center;
-  }
-
-  .divider {
-    display: flex;
-    justify-content: center;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 20px;
-  }
-
-  .project {
-    background-color: #272727;
-    display: flex;
-    width: 480px;
-    flex-direction: column;
-    border-radius: 10px;
-    font-size: 16px;
-    padding: 10px 15px;
-    gap: 5px;
-  }
-
-  .titleProject {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 20px;
-    font-weight: bold;
-  }
-
-  .tools {
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
-    flex-wrap: wrap;
-    align-items: center;
-    margin-top: 10px;
-  }
-
-  .tool {
-    padding: 5px;
-    border-radius: 10px;
-    color: #fff;
-    background-color: #3f3f3f;
-  }
-
-  .buttons {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    flex-direction: row;
-    margin-top: 10px;
-  }
-
-  button {
-    color: #272727;
-    border-radius: 10px;
-    border: none;
-    font-weight: bold;
-    padding: 10px;
-    font-size: 16px;
-    background-color: #fff;
-    cursor: pointer;
-  }
-
-  @media ${device.tablet} {
-    padding: 10px 13px;
-    width: 90%;
-
-    .projectsTitle {
-      font-size: 28px;
-    }
-
-    .project {
-      width: 350px;
-      padding: 10px;
-      font-size: 20px;
-    }
-
-    .tools {
-      font-size: 18px;
-    }
-
-    .buttons {
-      font-size: 16px;
-    }
+  .image {
+    border-radius: 50%;
   }
 `;
