c40d5baad3ad33340ba7434e86368c8e30076988
Patrick <patrickburin17@gmail.com>
2023-12-05 17:48:16 -0300

    Desenvolvimento das experiencias e projetos

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,10 +6,11 @@ import { FaWhatsapp, FaGithub } from "react-icons/fa";
 import { IoLogoVercel } from "react-icons/io5";
 import { CiLinkedin } from "react-icons/ci";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { GoLink } from "react-icons/go";
 
 const Home = () => {
   const [typeEffect] = useTypewriter({
-    words: ["TypeScript", "JavaScript", "React"],
+    words: ["TypeScript", "JavaScript", "React", "Node.js"],
     loop: 0,
     typeSpeed: 100,
     deleteSpeed: 100,
@@ -29,36 +30,178 @@ const Home = () => {
         </C.Developer>
 
         <C.SocialMedia>
-          <div className="icon">
-            <FaWhatsapp size={50} color="#25D366" />
-          </div>
-          <div className="icon">
+          <div
+            className="icon"
+            onClick={() => {
+              window.open("https://github.com/patrickburin", "blank");
+            }}
+          >
             <FaGithub size={50} />
           </div>
-          <div className="icon">
-            <IoLogoVercel size={50} />
-          </div>
-          <div className="icon">
+          <div
+            className="icon"
+            onClick={() => {
+              window.open(
+                "https://www.linkedin.com/in/patrick-burin-rodriguez-398496206/",
+                "blank"
+              );
+            }}
+          >
             <CiLinkedin size={50} />
           </div>
-          <div className="icon">
+          <div
+            className="icon"
+            onClick={() => {
+              window.open(
+                "https://vercel.com/patrick-burin-rodriguezs-projects",
+                "blank"
+              );
+            }}
+          >
+            <IoLogoVercel size={50} />
+          </div>
+          <div
+            className="icon"
+            onClick={() => {
+              window.open("https://www.instagram.com/patrickburinn/", "blank");
+            }}
+          >
             <img src={logoInstagram} />
+          </div>
+          <div
+            className="icon"
+            onClick={() => {
+              window.open(
+                "https://api.whatsapp.com/send?phone=554899082788",
+                "blank"
+              );
+            }}
+          >
+            <FaWhatsapp size={50} color="#25D366" />
           </div>
         </C.SocialMedia>
       </C.Start>
 
-      <C.Experiences>
+      <C.AboutMe>
         <img src={profile} className="image" />
-
         <div className="descripton">
-          <div className="title">Olá, eu sou o Patrick!</div>
+          <div className="titleName">Olá, eu sou o Patrick!</div>
           Estou cursando a 7ª fase de Ciência da Computação e minha vida pessoal
           é bastante variada. Gosto bastante de treinar e esportes,
           principalmente futebol, handebol e vôlei. Também gosto de jogar alguns
           eSports e assistir séries e filmes. Tenho o sonho de viajar pelo mundo
           conhecendo vários países e culturas diferentes.
         </div>
+      </C.AboutMe>
+
+      <C.Experiences>
+        <div className="title">Minhas experiências</div>
+        <div className="experience">
+          <div className="top">
+            <div className="titleExperience">
+              Desenvolvedor front end na{" "}
+              <a href="https://oaiscloud.com/" target="_blank">
+                OAIS Cloud
+              </a>{" "}
+              / Unesc Labs
+            </div>
+            <div className="dateExperience">Fevereiro 2023 - Hoje</div>
+          </div>
+          <div className="content">
+            A OAIS Cloud é uma empresa de tecnologia que atua no mercado de
+            armazenamento de dados na nuvem e acesso à assinatura digital.
+            Atuando como desenvolvedor front end, meu papel na empresa é o
+            desenvolvimento do portal de acesso às ferramentas da empresa com
+            foco na intuitividade para uma melhor experiência do usuário.
+          </div>
+          <div className="tools">
+            Tecnologias:
+            <div className="tool">TypeScript</div>
+            <div className="tool">React</div>
+            <div className="tool">Styled Components</div>
+            <div className="tool">Material UI</div>
+            <div className="tool">Figma</div>
+            <div className="tool">GitHub</div>
+          </div>
+        </div>
+        <div className="experience">
+          <div className="top">
+            <div className="titleExperience">
+              Suporte técnico na{" "}
+              <a href="https://www.consolidesuamarca.com.br/" target="_blank">
+                Consolide Registro de Marcas
+              </a>
+            </div>
+            <div className="dateExperience">Fevereiro 2021 - Abril 2022</div>
+          </div>
+          <div className="content">
+            A Consolide é a maior empresa de registro de marcas do Brasil.
+            Atuando desde 2013 no mercado, a empresa tem o papel de auxiliar os
+            clientes com toda a burocracia de um registro de marca. Atuando como
+            suporte técnico, meu papel na empresa era desde a manutenção no
+            hardware, até a comunicação entre clientes internos e
+            desenvolvedores, para um melhor entendimento do problema e uma maior
+            agilidade na resolução.
+          </div>
+          <div className="tools">
+            Tecnologias:
+            <div className="tool">Basecamp</div>
+            <div className="tool">Trello</div>
+          </div>
+        </div>
       </C.Experiences>
+
+      <C.Projects>
+        <div className="title">Projetos</div>
+        <div className="projects">
+          <div className="project">
+            <div className="titleProject">
+              Estofaria do Ananias <GoLink size={30} />
+            </div>
+            Site de uma estofaria especializada em carros antigos
+            <div className="contentProject">
+              <div className="tool">TypeScript</div>
+              <div className="tool">React</div>
+              <div className="tool">Figma</div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="titleProject">
+              Jogo adivinhar bandeira <GoLink size={30} />
+            </div>
+            Jogo de adivinhar bandeiras de países
+            <div className="contentProject">
+              <div className="tool">TypeScript</div>
+              <div className="tool">React</div>
+              <div className="tool">Java</div>
+              <div className="tool">Spring Boot</div>
+              <div className="tool">Sockets</div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="titleProject">
+              Estofaria do Ananias <GoLink size={30} />
+            </div>
+            Site de uma estofaria especializada em carros antigos
+            <div className="contentProject">
+              <div className="tool">TypeScript</div>
+              <div className="tool">React</div>
+              <div className="tool">Figma</div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="titleProject">
+              Estofaria do Ananias <GoLink size={30} />
+            </div>
+            Site de uma estofaria especializada em carros antigos
+            <div className="contentProject">
+              <div className="tool">TypeScript</div>
+              <div className="tool">React</div>
+              <div className="tool">Figma</div>
+            </div>
+          </div>
+        </div>
+      </C.Projects>
     </C.Container>
   );
 };
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-// import { device } from "../../device";
+import { device } from "../../device";
 
 export const Container = styled.div`
   display: flex;
@@ -24,13 +24,20 @@ export const Start = styled.div`
   align-items: center;
   justify-content: center;
   gap: 50px;
-  height: 100vh;
+  min-height: 100vh;
 
   .name {
     text-align: center;
     color: #fff;
     font-weight: normal;
     font-size: 70px;
+  }
+
+  @media ${device.tablet} {
+    height: 100vh;
+    .name {
+      font-size: 50px;
+    }
   }
 `;
 
@@ -41,6 +48,16 @@ export const Developer = styled.div`
 
   span {
     color: #ff0000;
+  }
+
+  @media ${device.tablet} {
+    font-size: 28px;
+  }
+
+  @media ${device.mobile} {
+    flex-direction: column;
+    align-items: center;
+    gap: 0;
   }
 `;
 
@@ -55,7 +72,7 @@ export const SocialMedia = styled.div`
   }
 `;
 
-export const Experiences = styled.div`
+export const AboutMe = styled.div`
   display: flex;
   flex-direction: row;
   width: 90%;
@@ -65,13 +82,197 @@ export const Experiences = styled.div`
   padding: 20px;
   gap: 30px;
 
+  .titleName {
+    font-size: 40px;
+    font-weight: bold;
+  }
+
   .descripton {
     display: flex;
     flex-direction: column;
     gap: 5px;
+    text-align: justify;
   }
 
   .image {
     border-radius: 50%;
+  }
+
+  @media ${device.tablet} {
+    flex-direction: column;
+
+    .titleName {
+      font-size: 30px;
+      text-align: center;
+    }
+
+    .descripton {
+      font-size: 22px;
+      padding: 5px 10px;
+    }
+  }
+`;
+
+export const Experiences = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  min-height: 100vh;
+  font-size: 40px;
+  align-items: center;
+  font-weight: bold;
+  gap: 15px;
+  color: #000;
+
+  .title {
+    color: #fff;
+  }
+
+  a {
+    color: #000;
+  }
+
+  .experience {
+    display: flex;
+    width: 95%;
+    font-size: 32px;
+    flex-direction: column;
+    gap: 10px;
+    background-color: #fff;
+    border-radius: 30px;
+    padding: 20px;
+  }
+
+  .top {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .titleExperience {
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  .dateExperience {
+    font-size: 18px;
+    font-weight: normal;
+  }
+
+  .content {
+    font-size: 18px;
+    font-weight: normal;
+    text-align: justify;
+  }
+
+  .tools {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    font-size: 20px;
+    gap: 10px;
+  }
+
+  .tool {
+    display: flex;
+    background-color: #222;
+    padding: 5px 10px;
+    border-radius: 15px;
+    color: #fff;
+  }
+
+  @media ${device.tablet} {
+    width: 85%;
+
+    .title {
+      font-size: 30px;
+    }
+
+    .titleExperience {
+      font-size: 22px;
+    }
+
+    .content {
+      font-size: 20px;
+    }
+  }
+`;
+
+export const Projects = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 90%;
+  min-height: 100vh;
+  gap: 15px;
+  font-size: 40px;
+  font-weight: bold;
+  text-align: center;
+  color: #000;
+
+  .title {
+    color: #fff;
+  }
+
+  .projects {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 20px;
+    cursor: pointer;
+    justify-content: space-between;
+  }
+
+  .project {
+    display: flex;
+    width: 45%;
+    text-align: start;
+    flex-direction: column;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 10px 15px;
+    font-size: 18px;
+    font-weight: normal;
+    gap: 10px;
+  }
+
+  .titleProject {
+    display: flex;
+    font-size: 22px;
+    justify-content: space-between;
+    font-weight: bold;
+  }
+
+  .contentProject {
+    font-size: 18px;
+    font-weight: normal;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .tool {
+    display: flex;
+    background-color: #222;
+    padding: 5px 10px;
+    border-radius: 15px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+  @media ${device.laptop} {
+    .project {
+      width: 100%;
+    }
+  }
+
+  @media ${device.tablet} {
+    .title {
+      font-size: 30px;
+    }
   }
 `;
